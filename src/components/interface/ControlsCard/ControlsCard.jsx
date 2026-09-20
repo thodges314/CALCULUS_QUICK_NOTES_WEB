df0d7da652e3d6965da91266e3b27fd003d7325f
Thomas Hodges <thodges@javascriptonline.org>
2026-09-20 03:17:43 -0700

    refactor: simplify ControlsCard and CustomCheckbox components by removing Material-UI dependencies

--- src/components/interface/ControlsCard/ControlsCard.jsx
+++ src/components/interface/ControlsCard/ControlsCard.jsx
@@ -1,39 +1,7 @@
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-
-import { styled } from "@mui/material/styles";
-import { hexToRgba } from "utils/utils";
-import {
-  synthCyberBlack,
-  synthCyberPink,
-  synthSunsetPink,
-} from "interactivity/resources/constants/colors";
-
-const darkColor = hexToRgba(synthCyberBlack, 0.8);
-const cyberPink = hexToRgba(synthCyberPink);
-const sunsetPink = hexToRgba(synthSunsetPink);
-
-const StyledControlsCard = styled(Card)({
-  width: "100%",
-  marginTop: "20px",
-  color: cyberPink,
-  backgroundColor: darkColor,
-  boxShadow: `2px 2px 2px 2px ${cyberPink}`,
-  borderLeft: `1px solid ${sunsetPink}`,
-  borderRight: `1px solid ${sunsetPink}`,
-  borderBottom: `1px solid ${sunsetPink}`,
-  borderTop: `1px solid ${sunsetPink}`,
-
-  "& > div": {
-    display: "flex",
-    flexDirection: "column",
-  },
-});
-
 const ControlsCard = ({ children }) => (
-  <StyledControlsCard>
-    <CardContent>{children}</CardContent>
-  </StyledControlsCard>
+  <div className="w-full mt-5 rounded border border-synth-sunset-pink bg-synth-cyber-black/80 text-synth-cyber-pink shadow-[2px_2px_2px_2px_#de41cd]">
+    <div className="flex flex-col p-4 pb-6">{children}</div>
+  </div>
 );
 
 export default ControlsCard;
--- src/components/interface/CustomCheckbox/CustomCheckbox.jsx
+++ src/components/interface/CustomCheckbox/CustomCheckbox.jsx
@@ -1,14 +1,8 @@
 import Checkbox from "@mui/material/Checkbox";
-import { styled } from "@mui/material/styles";
-import { hexToRgba } from "utils/utils";
 
-import { synthCyberPink } from "interactivity/resources/constants/colors";
-
-const cyberPink = hexToRgba(synthCyberPink);
-
-export const CustomCheckbox = styled(Checkbox)({
-  color: cyberPink,
-  "&.Mui-checked": {
-    color: cyberPink,
-  },
-});
+export const CustomCheckbox = (props) => (
+  <Checkbox
+    {...props}
+    className="text-synth-cyber-pink! [&.Mui-checked]:text-synth-cyber-pink!"
+  />
+);
